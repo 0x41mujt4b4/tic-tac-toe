e201fc6cd37cb3830b77d8f9365e3b060fda6052
almujtaba007 <almujtaba007@gmail.com>
2021-07-25 17:36:53 +0200

    enabled buttons

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ class Board extends React.Component {
       );
     }
     return (
-    <div>{rows}</div>
+    <div className='board' >{rows}</div>
     );
   }
 }
@@ -140,18 +140,18 @@ class Game extends React.Component {
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
-    // const moves = history.map((step, move) => {
-    //   const desc = move ?
-    //   'Go to move #' + move :
-    //   'Go to game start';
-    //   return (
-    //     <li key={move} >
-    //       <button onClick={() => this.jumpTo(move)}>
-    //         {desc}
-    //       </button>
-    //     </li>
-    //   );
-    // });
+    const moves = history.map((step, move) => {
+      const desc = move ?
+      'Go to move #' + move :
+      'Go to game start';
+      return (
+        <li key={move} >
+          <button onClick={() => this.jumpTo(move)}>
+            {desc}
+          </button>
+        </li>
+      );
+    });
 
     let status;
     if (winner) {
@@ -173,7 +173,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          {/* <ol>{moves}</ol> */}
+          <ol>{moves}</ol>
         </div>
       </div>
     );
